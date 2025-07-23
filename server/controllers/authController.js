869589b6db628bc4a3711157a965fc9080bcb213
Shivayan Chakravarty <your_email@example.com>
2025-07-23 23:39:11 +0530

    minor issues

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,7 @@ import bcrypt, { hash } from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import userModel from '../models/userModel.js';
 import transporter from '../config/nodemailer.js';
-
+import { EMAIL_VERIFY_TEMPLATE, PASSWORD_RESET_TEMPLATE } from "../config/emailTemplates.js";
 
 export const register = async(req, res) => {
     const {name, email, password} = req.body;
